d71c4d0a3f33a17d20550c91c95a5e9ec15bf931
Aluno <aluno@embarquedigital.portodigital.local>
2022-09-30 16:29:35 -0300

    mais ajustes nos comentarios

--- comentarios/code.js
+++ comentarios/code.js
@@ -1,27 +1,17 @@
-// var post= document.getElementById("post");
-// post.addEventListener("click", function(){
-//     var commentBoxValue= document.getElementById("comment-box").value;
- 
-//     var li = document.createElement("li");
-//     var text = document.createTextNode(commentBoxValue);
-//     li.appendChild(text);
-//     document.getElementById("unordered").appendChild(li);
- 
-// });
-
-
-window.addEventListener("load", function(){
-   document.querySelector('#question-generator').addEventListener('click', createComment);
+window.addEventListener("load", function () {
+    const questionButton = document.querySelector('#question-generator');
+    questionButton.addEventListener('click', createComment);
+    questionButton.addEventListener('click', () => questionButton.classList.add("invisible"))
 });
 
-function createComment(){
+function createComment() {
 
     const body = document.querySelector('body');
     const container = document.querySelector('#container');
-    container.removeEventListener('click',createComment);
+    container.removeEventListener('click', createComment);
     const button = document.querySelector('#question-generator');
 
-    
+
     const div1 = document.createElement('div');
     div1.classList.add('item');
     div1.setAttribute("id", "title-text-wrapper");
@@ -47,11 +37,48 @@ function createComment(){
     postButton.setAttribute("id", "post");
     postButton.textContent = 'Postar';
 
+    postButton.addEventListener("click", function () {
+        if (!(commentBox.value == null || titleBox.value == null)) {
+            const commentDiv = document.createElement("div");
+            commentDiv.classList.add('item');
+
+            const commentBoxValue = commentBox.value;
+            const titleBoxValue = titleBox.value;
+
+            commentBox.value = "";
+            titleBox.value = "";
+
+
+            const title = document.createElement("p");
+            const titleText = document.createTextNode(titleBoxValue);
+            title.appendChild(titleText);
+            document.getElementById
+
+            const comment = document.createElement("p");
+            const text = document.createTextNode(commentBoxValue);
+            comment.appendChild(text);
+
+            commentDiv.appendChild(title);
+            commentDiv.appendChild(comment);
+
+            document.getElementById("container").appendChild(commentDiv);
+        }
+
+
+
+    });
+
     const cancelButton = document.createElement('button');
     cancelButton.setAttribute("id", "cancel");
     cancelButton.textContent = 'Cancelar';
 
-    button.remove();
+    cancelButton.addEventListener("click", () => {
+        div1.remove();
+        div2.remove();
+        button.classList.remove("invisible");
+    });
+
+
     div1.appendChild(titleText);
     div1.appendChild(titleBox);
     div1.appendChild(commentText);
@@ -61,6 +88,5 @@ function createComment(){
     div2.appendChild(postButton);
     div2.appendChild(cancelButton);
     container.appendChild(div2);
-
 
 }
