5f8f000939354bba8846e5a2d2becc3e1a488afd
ruukaas <lucashenriquhca@gmail.com>
2022-10-09 15:59:04 -0300

    Criando classes de trilha, modulo e lesson e salvando as informações dos inputs nas classes

---
+++ telaAdmTrilha2/code.js
@@ -0,0 +1,67 @@
+document.getElementsByClassName("fa-solid fa-circle-arrow-right fa-3x")[0].addEventListener("click", () => {
+    let title = getTitleTrilha()
+    let residenceStage = getResidenceStage()
+    currentTrilha = new trilha(title, residenceStage)
+    console.log(currentTrilha)
+})
+
+let currentTrilha
+
+const selectMarkedCheckbox = () => {
+    let markedCheckbox = []
+    let checkbox = Array.prototype.slice.call(document.getElementsByClassName("itemTrilha mousePointer"))
+    checkbox.forEach(element => {
+        if(element.checked) {
+            markedCheckbox.push(element)
+        }
+    })    
+    return markedCheckbox
+}
+
+const getResidenceStage = (checkBoxArrayEl = selectMarkedCheckbox()) => {
+    let residenceStage = []
+    checkBoxArrayEl.forEach(element => {
+        residenceStage.push(element.parentNode.getElementsByTagName("p")[0].textContent)
+    })
+    return residenceStage
+}
+
+const getTitleTrilha = () => {
+    let input = document.getElementById("textInput")
+    return input.value
+}
+
+class trilha {
+    constructor(title, residenceStageArray) {
+        this.title = title
+        this.residenceStage = residenceStageArray
+    }
+
+    modules = []
+
+    setModules(modulo) {
+        this.modules.push(modulo)
+    }
+} 
+
+class modulo {
+    constructor(title) {
+        this.title = title
+    }
+
+    lessons = []
+    setLessons(lesson) {
+        this.lessons.push(lesson)
+    }
+}
+
+
+
+class lesson {
+    constructor(lessonTitle,videoType,link) {
+        this.lessonTitle = lessonTitle
+        this.videoType = videoType
+        this.link =  link
+    }
+}
+
