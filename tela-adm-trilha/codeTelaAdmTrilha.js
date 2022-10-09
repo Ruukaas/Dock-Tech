44d97fce5a3194bc49242fb6773791ef2443ebe7
fernando-castelo <nando.castelobranco24@hotmail.com>
2022-10-09 17:22:41 -0300

    adicao do modal de exclusao de trilhas

--- tela-adm-trilha/codeTelaAdmTrilha.js
+++ tela-adm-trilha/codeTelaAdmTrilha.js
@@ -1,6 +1,25 @@
-const addButton = document.querySelector("#addButton");
-addButton.addEventListener("onclick", redirecionar);
+// Get the modal
+var modal = document.getElementById("myModal");
 
-function redirecionar(){window.location.href = "/telaAdmTrilha2.html"
-};
-setTimeout(redirecionar, 3000);
+// Get the button that opens the modal
+var btn = document.getElementById("deleteButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
