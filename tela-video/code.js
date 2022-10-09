f02a508daffc9f73665da0ac91a1d9678c836aa5
ruukaas <lucashenriquhca@gmail.com>
2022-10-08 22:23:46 -0300

    Consertando erros de saveStats e adicionando o changeTab e a checkbox

--- tela-video/code.js
+++ tela-video/code.js
@@ -61,17 +61,30 @@ let currentSecondsVideoPlayer //O tempo salvo do video no momento
 let currentTypeVideo //O tipo do vídeo do momento
 let progressTrilha // Porcentagem da trilha que foi concluída
 let isCurrentVideoFinished //boolean que vai indicar se o vídeo do momento foi concluído
-let percentageCompletedCurrentVideo //Porcentagem do vídeo atual
 let percentageCompletedTrilha //Porcentagem da trilha
 let currentVideoPlayerTimeComplete //Temp em segundos do vídeo completo 
-let arrayLessons = [] //Array com todas as lessons que vai ser usado para passar para a próxima
+let arrayLessons = []//array com todas as aulas da trilha corrente
+let isLessonsConcludedArray = [] //array que vai dizer na posição da vídeo aula se aquele vídeo foi concluído ou não 
+let currentPositionOrder 
 
 
-trilha.modules.forEach (valor => {
+trilha.modules.forEach(valor => {
     valor.lessons.forEach(valor => {
         arrayLessons.push(valor)
+        isLessonsConcludedArray.push(false) //Por padrão cada posição do array é falsa, é colocado aqui para que ele tenha a mesma quantidade de falses da quantidade de aula
     })
 })
+
+const pushLessonConcludedArray = (position) => {
+    isLessonsConcludedArray[position] = true
+    console.log(isLessonsConcludedArray)
+}
+
+const isTrilhaCompleted = () => {
+    let isCompleted = false
+    isCompleted = isLessonsConcludedArray.every(valor => valor)
+    return isCompleted
+}
 
 //API Script
 let tag = document.createElement('script');
@@ -89,11 +102,18 @@ function onYouTubeIframeAPIReady() {
     reBuildIframe(firstLessonLink,firstLessonVideoType)
     changeCurrentTypeVideo(firstLessonVideoType)
     changeVideoTitle(firstLessonTitle)
-    
+    changeCurrentPositionOrder(0)
 }
 
-//DOM
+const changeCheckBox = () => {
+    
+    document.getElementsByName(currentPositionOrder)[0].setAttribute("checked","checked")
+}
+
+let countPositionOrder
+//DOM Video/Trilha
 const dynamicTrilha = (trilhaJSON) => {
+    countPositionOrder = 0
     const trilhaEL = document.getElementById("trilha")
     let summaryEL = document.createElement("ol")
     summaryEL.classList.add("summary")
@@ -105,13 +125,16 @@ const dynamicTrilha = (trilhaJSON) => {
 
         contentModuleArray.forEach((value) => {
             let lesson = createLesson(value)
+            let checkbox = createCheckbox()
+
+            lesson.appendChild(checkbox)
+            countPositionOrder++
             subModule.appendChild(lesson)
         })
 
         module.appendChild(subModule)
         summaryEL.appendChild(module)
     })
-
     trilhaEL.appendChild(summaryEL)
 }
 
@@ -130,32 +153,103 @@ const createLessonsModule = () => {
 }
 
 const createLesson = (lesson) => {
+    let div = document.createElement("div")
+    div.classList.add("lesson")
     let li = document.createElement("li")
-    li.classList.add("lesson")
     li.textContent = lesson.lessonTitle
-    li.addEventListener("click", () => {
+    li.setAttribute("id", countPositionOrder) 
+    li.addEventListener("click", (eventTarget) => {
+        changeCurrentPositionOrder( Number(eventTarget.target.id))
         saveCurrentStats()
         deletePreviousVideo()
         changeCurrentTypeVideo(lesson.videoType)
         reBuildIframe(lesson.link, lesson.videoType)
         changeVideoTitle(lesson.lessonTitle)
     })
-
-    return li;
+    div.appendChild(li)
+    return div;
 }
 
-const saveCurrentStats = () => {
-    switch (currentTypeVideo) {
+const createCheckbox = () => {
+    let checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.setAttribute("name", countPositionOrder)
+    checkbox.setAttribute("disabled","disabled")
+    
+    return checkbox
+}
+
+const changeCurrentPositionOrder = position => currentPositionOrder = position
+
+const reBuildIframe = (link, videoType) => {
+    switch (videoType) {
         case "external":
-            changeCurrentSecondsVideoPlayer(Math.ceil(player.getCurrentTime()))
-            console.log("extStas" + currentSecondsVideoPlayer)
-            
+            buildExternalVideo(link.slice(30)) //A partir do 30 só fica o ID do link
             break
         case "internal":
-            console.log(player.currentTime)
-            changeCurrentSecondsVideoPlayer(Math.ceil(player.currentTime))
-            console.log("intStats" + currentSecondsVideoPlayer)
-            break;
+            buildInternalVideo(link)
+            break
+    }
+}
+
+const buildExternalVideo = videoID => {
+    player = new YT.Player('video-external', {
+        height: '720',
+        width: '1280',
+        //Primeiro vídeo da trilha
+        videoId: videoID,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    })
+}
+
+const buildInternalVideo = link => {
+
+    let isCurrentTimeSaved = false
+
+    player = document.createElement("video")
+    player.setAttribute("src", link)
+    player.setAttribute("width", 1280)
+    player.setAttribute("height", 720)
+    player.setAttribute("controls", "true")
+    player.setAttribute("autoplay","autoplay")
+
+    player.addEventListener("play", () => {
+        if(!isCurrentTimeSaved) {
+            changecurrentVideoPlayerTimeComplete()
+            !!isCurrentTimeSaved
+        }
+        saveCurrentStats()
+    })
+    player.addEventListener("pause", saveCurrentStats )
+    player.addEventListener("ended", () => {
+        currentSecondsVideoPlayer = currentVideoPlayerTimeComplete
+        console.log(currentPositionOrder)
+        pushLessonConcludedArray(currentPositionOrder)
+        changeCheckBox()
+
+    } )
+    
+    document.getElementById("video-internal").appendChild(player)
+}
+
+const onPlayerEnded = (position) => {
+
+}
+
+const onPlayerReady = () => {
+    player.playVideo()
+    changecurrentVideoPlayerTimeComplete()
+}
+
+const onPlayerStateChange = (objectEvent) => {
+    saveCurrentStats()
+    if(objectEvent.data == YT.PlayerState.ENDED) {
+        console.log("cabo")
+        pushLessonConcludedArray(currentPositionOrder)
+        changeCheckBox(currentPositionOrder)
     }
 }
 
@@ -171,83 +265,26 @@ const deletePreviousVideo = () => {
     player = null
 }
 
-const changeCurrentTypeVideo = (videoType) => {
-    currentTypeVideo = videoType
-}
-
-const reBuildIframe = (link, videoType) => {
-    switch (videoType) {
-        case "external":
-            buildExternalVideo(link.slice(30)) //A partir do 30 só fica o ID do link
-            break
-        case "internal":
-            buildInternalVideo(link)
-            break
-    }
-}
-
 const changeVideoTitle = title => {
     document.getElementById("video-player-title").textContent = title;
 }
 
-
-const buildExternalVideo = videoID => {
-    player = new YT.Player('video-external', {
-        height: '720',
-        width: '1280',
-        //Primeiro vídeo da trilha
-        videoId: videoID,
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': saveCurrentStats
-        }
-    })
-}
-
-const onPlayerReady = () => {
-    player.playVideo()
-}
-
-const onPlayerStateChange = () => {
-    saveCurrentStats()
-    if(player.data == YT.PlayerState.ENDED) {
-        changePercentageCurrentVideo()
-        nextVideo()
-    }
-    if(player.data == YT.PlayerState.PLAYING) {
-        changecurrentVideoPlayerTimeComplete()
-        changePercentageCurrentVideo()
+//Control and Save Status
+const saveCurrentStats = () => {
+    switch (currentTypeVideo) {
+        case "external":
+            changeCurrentSecondsVideoPlayer(Math.ceil(player.getCurrentTime()))
+            console.log("extStas" + currentSecondsVideoPlayer)      
+            break
+        case "internal":
+            changeCurrentSecondsVideoPlayer(Math.ceil(player.currentTime))
+            console.log("intStats" + currentSecondsVideoPlayer)
+            break;
     }
 }
 
-const nextVideo = () => {
-
-}
-
-
-const buildInternalVideo = link => {
-    player = document.createElement("video")
-    player.setAttribute("src", link)
-    player.setAttribute("width", 1280)
-    player.setAttribute("height", 720)
-    player.setAttribute("controls", "true")
-    player.setAttribute("autoplay","autoplay")
-
-    player.addEventListener("play", () => {
-        saveCurrentStats()
-        changecurrentVideoPlayerTimeComplete()
-        changePercentageCurrentVideo()
-     })
-    player.addEventListener("pause", saveCurrentStats )
-    player.addEventListener("ended", () => {
-        currentSecondsVideoPlayer = currentVideoPlayerTimeComplete
-        changePercentageCurrentVideo()
-        nextVideo()
-    } )
-    
-    document.getElementById("video-internal").appendChild(player)
-
-    
+const changeCurrentTypeVideo = (videoType) => {
+    currentTypeVideo = videoType
 }
 
 // const isExternal = lesson => {
@@ -268,16 +305,12 @@ window.addEventListener("beforeunload", saveCurrentStats)
 
 //Progress bar
 
-//percent/100 * all = part | percent = part *100/all ?
-const percentageCalc = (part = currentSecondsVideoPlayer, all = currentVideoPlayerTimeComplete) => {
+//percent/100 * all = part | percent = part *100/all 
+const percentageCalc = (part , all) => {
     let percent = ((part * 100)/all).toFixed(2)
     return percent
 }
 
-const changePercentageCurrentVideo = () => {
-    percentageCompletedCurrentVideo = percentageCalc()
-    console.log("pct|" + percentageCompletedCurrentVideo )
-}
 
 const changeCurrentSecondsVideoPlayer = sec => {
     currentSecondsVideoPlayer = sec
@@ -294,6 +327,56 @@ const changecurrentVideoPlayerTimeComplete = () => {
             console.log("intComplete|" +  currentVideoPlayerTimeComplete)
             break;
     }
+}
+
+//tabs
+
+const changeTabs = (eventTarget) => {
+    removeAllTabActive()
+    removeAllContentTab()
+    setTabActive(eventTarget.target.parentNode)
+}
+
+
+const removeAllContentTab = () => {
+    document.getElementById("append").innerHTML = ""
+}
+
+const tabs = Array.prototype.slice.call(document.getElementsByClassName("item"))
+
+tabs.forEach(valor => {
+    valor.addEventListener("click", changeTabs)
+})
+
+const setTabActive = element => element.classList.add("active")
+
+const removeAllTabActive = () => {
+    tabs.forEach(valor => valor.classList.remove("active"))
+}
+
+const setContentOfContentTab = (tab) => {
+
+
+    switch(tab) {
+        case "comments":
+
+        break
+        case "exercises":
+
+        break
+        case "notes":
+
+        break
+        case "comments":
+
+        break
+    }
+}
+
+let contentTabEl = document.getElementById("append")
+
+const setComments = () => {
+    
 }
 
 
