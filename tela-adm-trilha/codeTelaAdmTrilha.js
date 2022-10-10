4ea06c88aaceb6362cc606bc6b72cfdc82e91b81
fernando-castelo <nando.castelobranco24@hotmail.com>
2022-10-10 14:05:50 -0300

    ajustes modal

--- tela-adm-trilha/codeTelaAdmTrilha.js
+++ tela-adm-trilha/codeTelaAdmTrilha.js
@@ -1,19 +1,31 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("deleteButton");
+const deleteButton = document.getElementById("deleteButton");
+
+const confirmDelete = document.querySelector(".modalDelete");
+
+const cancelDelete = document.querySelector(".modalCancel");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+deleteButton.onclick = function() {
+  modal.style.display = "flex";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+  modal.style.display = "none";
+}
+
+confirmDelete.onclick = function() {
+  modal.style.display = "none";
+}
+
+cancelDelete.onclick = function() {
   modal.style.display = "none";
 }
 
