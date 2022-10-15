8f39202297fa0e42baf4e5553418f922cf2b063d
WemmersonAlb <wemmerson.albuquerque@gmail.com>
2022-10-15 11:14:05 -0300

    Update modal

--- tela-adm-trilha/codeTelaAdmTrilha.js
+++ tela-adm-trilha/codeTelaAdmTrilha.js
@@ -6,8 +6,9 @@ function next(){
 }
 
 const body = document.getElementsByTagName("body");
-// Get the modal
+// Get the modal and fade
 const modal = document.getElementById("myModal");
+const fade = document.getElementById("fade");
 
 // Get the button that opens the modal
 const deleteButton = document.getElementById("deleteButton");
@@ -24,17 +25,20 @@ const span = document.getElementById("close");
 
 // When the user clicks on the button, open the modal
 deleteButton.onclick = function() {
-  modal.style.display = "flex";
+  modal.style.display = "flex";  
+  fade.style.display = "flex";
   liSelecionada.style.display = "none";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  fade.style.display = "none";
 }
 
 document.getElementById("modalDelete").onclick = function() {
   modal.style.display = "none";
+  fade.style.display = "none";
   liSelecionada = deleteButton.parentNode.getElementsByClassName(".itemTrilha");
   console.log(liSelecionada);
   liSelecionada.parentNode.removeChild(liSelecionada);
@@ -42,11 +46,13 @@ document.getElementById("modalDelete").onclick = function() {
 
 document.getElementById("modalCancel").onclick = function() {
   modal.style.display = "none";
+  fade.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    fade.style.display = "none";
   }
 }
