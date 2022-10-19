68b74eb33b65cd26d012c54c824cd69a814ad0ed
fernando-castelo <nando.castelobranco24@hotmail.com>
2022-10-19 19:23:55 -0300

    tela-adm-squads inicial

---
+++ tela-adm-squads/all-squads.js
@@ -0,0 +1,18 @@
+//Arquivo simulando a lista de trilhas cadastradas no sistema
+export let allTrilhas = [
+    {
+        title: "Squad 1 - Rise Up!"
+    },
+    {
+        title: "Squad 2 - Kick Off!"
+    },
+    {
+        title: "Squad 3 - Rise Up!"
+    }, 
+    {
+        title:"Squad 4 - Grow Up!"
+    }, 
+    {
+        title:"Squad 5 - Kick Off!"
+    }
+]
