b4b8d800a811fe6c98a278f3a942c4a9bcc980fa
fernando-castelo <nando.castelobranco24@hotmail.com>
2022-10-22 01:01:43 -0300

    tela squad passo 3

---
+++ tela-adm-squads-passo3/code.js
@@ -0,0 +1,5 @@
+let nextButton = document.querySelector(".next-button");
+nextButton.addEventListener("click", back);
+function back(){
+  window.location.href = "../tela-adm-squads-passo2/tela-adm-squads-passo2.html";
+}
