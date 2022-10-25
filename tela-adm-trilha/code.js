a30d0d007043ad99f45f5372daa6383708251c7a
ruukaas <lucashenriquehca@gmail.com>
2022-10-25 17:45:54 -0300

    organizando código

--- tela-adm-trilha/code.js
+++ tela-adm-trilha/code.js
@@ -1,80 +1,58 @@
 //Dynamic List
-import {setContainerEl, dynamicList} from "../assets/code/list-item.js"
-import {allTrilhas} from "./allTrilhas-adm.js"
+import { setContainerEl, dynamicList } from "../assets/code/list-item.js"
+import { allTrilhas } from "./allTrilhas-adm.js"
 
 setContainerEl("containerTrilha")
-dynamicList(allTrilhas,"Trilhas","fonteCinza","lista-de-trilhas","itemTrilha")
+dynamicList(allTrilhas, "Trilhas", "fonteCinza", "lista-de-trilhas", "itemTrilha")
+
+//elements
+let addEl = document.getElementById("addButton");
 
 
 //botton add going to telaTrilha2
-let add = document.getElementById("addButton");
-add.addEventListener("click", next);
-function next(){
+const modalEl = document.getElementById("myModal");
+const fadeEl = document.getElementById("fade");
+const closeModalEl = document.getElementById("close");
+
+const next = () => {
   window.location.href = "../telaAdmTrilha2/telaAdmTrilha2.html";
 }
 
-const body = document.getElementsByTagName("body");
-// Get the modal and fade
-const modal = document.getElementById("myModal");
-const fade = document.getElementById("fade");
-
-// Get the button that opens the modal
-const deleteButton = document.getElementById("deleteButton");
-
-const confirmDelete = document.getElementById("modalDelete");
-
-let liSelecionada;
-
-
-const cancelDelete = document.getElementById("modalCancel");
-
-// Get the <span> element that closes the modal
-const span = document.getElementById("close");
-
-// // When the user clicks on the button, open the modal
-// deleteButton.onclick = function() {
-
-// }
-
 const openModal = () => {
-  modal.style.display = "flex";  
-  fade.style.display = "flex";
+  fadeEl.style.display = "flex";
+  modalEl.style.display = "flex";
 }
 
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-  fade.style.display = "none";
-}
-
-document.getElementById("modalDelete").onclick = function() {
-  modal.style.display = "none";
-  fade.style.display = "none";
-  liSelecionada = deleteButton.parentNode.getElementsByClassName(".itemTrilha");
-  console.log(liSelecionada);
-  liSelecionada.parentNode.removeChild(liSelecionada);
-}
-
-document.getElementById("modalCancel").onclick = function() {
-  modal.style.display = "none";
-  fade.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    fade.style.display = "none";
+const closeModal = () => {
+  closeModalEl.onclick = function () {
+    modalEl.style.display = "none";
+    fadeEl.style.display = "none";
   }
 }
 
-const openModalAddEvent = (buttonHTMLCollection,functionListener) => {
+const confirmActionModal = () => {
+  document.getElementById("modalConfirm").onclick = function () {
+    modalEl.style.display = "none";
+    fadeEl.style.display = "none";
+    //TODO deleter do banco primeiro depois apagar o modal
+  }
+}
+
+const declineActionModal = () => {
+  document.getElementById("modalCancel").onclick = function () {
+    modalEl.style.display = "none";
+    fadeEl.style.display = "none";
+  }
+}
+
+const openModalAddEvent = (buttonHTMLCollection, functionListener) => {
   let buttonsArray = Array.prototype.slice.call(buttonHTMLCollection)
   buttonsArray.forEach(elemento => {
     elemento.addEventListener("click", functionListener)
   })
 }
 
-openModalAddEvent(document.getElementsByClassName("delete-button"))
+openModalAddEvent(document.getElementsByClassName("delete-button"),openModal)
 
+
+addEl.addEventListener("click", next);
