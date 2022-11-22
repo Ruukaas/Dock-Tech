ea444ac1109907de06e2d6d029917a354f69d9e7
Ruukas <lalalalala>
2022-11-21 21:16:25 -0300

    teste 2

--- tela-adm-squads-inicial/code.js
+++ tela-adm-squads-inicial/code.js
@@ -3,7 +3,7 @@ import {setContainerEl, dynamicList} from "./list-squads.js"
 import {allSquads} from "./all-squads.js"
 import {createDivWithID, createDivWithClasses, createTitle, createImage} from "../assets/DOM.js"
 
-//teste
+//teste 2
 setContainerEl("containerTrilha")
 dynamicList(allSquads,"Squads","title","container-lista","lista")
 
