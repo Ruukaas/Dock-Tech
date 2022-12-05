036115e25049485958f9b1df80f141a52a051295
Ruukaas <lucashenriquehca@gmail.com>
2022-12-05 00:23:03 -0300

    redirecionando usuário para a página inicial dele

--- tela-de-login/js/buttonHandler.js
+++ tela-de-login/js/buttonHandler.js
@@ -3,12 +3,17 @@ import { filterByOneKey } from "../../assets/code/db/CRUD.js";
 import { app } from "../../assets/code/db/firebase.js"
 import { getInputValueByName } from "../../assets/code/DOM/DOM.js";
 
+
 window.onload = (e) => {
     e.preventDefault();
 }
 
 const button = document.getElementById('enter');
 const loadingImg = document.getElementById('loading-image');
+
+const goToTelaInicialUsuario = () => {
+    window.location.href = "../../tela-gerenciamento/tela-gerenciamento.html"
+}
 
 button.addEventListener('click', (e) => {
     if (e.target == button) {
@@ -23,28 +28,28 @@ button.addEventListener('click', (e) => {
                     // Signed in 
                     const user = userCredential.user;
                     let currentEmail = user.email
-                    let currentUser = await filterByOneKey("usuarios","email",[currentEmail])
+                    let currentUser = await filterByOneKey("usuarios", "email", [currentEmail])
                     currentUser = currentUser[0]
                     console.log(currentUser)
-                    switch(currentUser.funcao) {
+                    switch (currentUser.funcao) {
                         case "Aluno":
                             console.log("aluno")
-                        //colocar a função que redireciona para a página inicial do aluno
-                        break
+                            goToTelaInicialUsuario()
+                            break
                         case "Mentor":
                             console.log("mentor")
-                        //colocar a função que redireciona para a página inicial do mentor
-                        break
+                            //colocar a função que redireciona para a página inicial do mentor
+                            break
                         case "Administrador":
                             console.log("adm")
-                        //colocar a função que redireciona para a página inicial do mentor
-                        break
+                            //colocar a função que redireciona para a página inicial do mentor
+                            break
                     }
                 })
                 .catch((error) => {
                     const errorCode = error.code;
                     const errorMessage = error.message;
-                    alert(errorCode+ "|" + errorMessage)
+                    alert(errorCode + "|" + errorMessage)
                     loadingImg.style.display = "none"
                     button.style.display = 'block';
                 });
